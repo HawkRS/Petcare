7571bf32c61357e26f5e22a6ab605e2e3b1f35d1
HawkRs <ing.carlosglezhdez@hotmail.com>
2022-12-19 18:47:37 -0600

    Cambios en database para migrar y poblar todo de golpe

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(ContentSeeder::class);
+        $this->call(MarkersSeeder::class);
     }
 }
