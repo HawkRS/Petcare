0a96d571209dc3adc624dfbbb1a07fcae68a1805
HawkRs <ing.carlosglezhdez@hotmail.com>
2022-12-22 08:25:57 -0600

    Agregadas funciones de index

--- app/Http/Controllers/RockoController.php
+++ app/Http/Controllers/RockoController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Intervention\Image\ImageManager;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Content;
+use App\Helper;
+
 
 class RockoController extends Controller
 {
-    //
+    use SoftDeletes;
+
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
+
+  private $f = 'rocko.';
+
+  /**
+   * Show the rocko page content settings.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
+  public function index()
+  {
+    //dd('index smartbites');
+      //$ContentList = Content::where('page', 'rocko')->get();
+      $ContentList = DB::table('content')
+                ->where('page', 'rocko')
+                ->get();
+      //dd($ContentList);
+      return view($this->f.'index', [
+        'Contenidos' => $ContentList,
+      ]);
+  }
+
 }
