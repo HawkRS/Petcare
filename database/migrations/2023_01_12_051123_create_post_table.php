fe24d63db1a6637759f836037094ad7717cdedf9
HawkRS <ing.carlosglezhdez@hotmail.com>
2023-01-11 23:31:11 -0600

    Creada migracion para posts

---
+++ database/migrations/2023_01_12_051123_create_post_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+          $table->id();
+          $table->string('slug')->unique();
+          $table->string('title');
+          $table->text('body');
+          $table->text('banner');
+          $table->unsignedBigInteger('users_id');
+          $table->foreign('users_id')->references('id')->on('users');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
