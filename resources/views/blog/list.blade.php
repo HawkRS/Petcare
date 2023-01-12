64be8fb21fb19d20252830f0ee38c2daa9691be9
HawkRS <ing.carlosglezhdez@hotmail.com>
2023-01-11 23:32:24 -0600

    Vista index para admin posts

---
+++ resources/views/blog/list.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('pagina', 'Blog')
+
+@section('content')
+
+<div class="page-content">
+  <div class="container-fluid">
+
+    <!-- start page title -->
+    <div class="row">
+      <div class="col-12">
+        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+          <h2 class="mb-sm-0">Blog</h2>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</div>
