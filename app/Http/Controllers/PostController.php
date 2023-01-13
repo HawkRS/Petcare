94390fd28689163dfd4ea57ea897144b7180497c
HawkRS <ing.carlosglezhdez@hotmail.com>
2023-01-13 02:03:50 -0600

    Controlador y funciones de muestra para blog en parte publica

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Helper;
+use App\Post;
 
 class PostController extends Controller
 {
+
+      private $f = 'blog.public.';
+
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +19,26 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+      $PostsList = Post::all();
+      //dd($PostsList[0]->Autor->name);
+
+      return view($this->f.'list', [
+          'Posts' => $PostsList,
+        ]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+      $Post = Post::where('slug', $slug)->first();
+      //dd($Post);
+      return view($this->f.'show', [
+          'Post' => $Post,
+        ]);
     }
 
 
